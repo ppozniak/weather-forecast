ea68496c3fc28d0f7cfc0302b37c01e4b6e3fcd3
Patryk Pozniak <patrykpozniak@esggen.com>
2024-06-06 18:14:40 +0100

    nivo pie test

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,25 @@
-import {
-  LineChart,
-  Line,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-} from "recharts";
+import { ResponsivePie } from "@nivo/pie";
 
 function App() {
-  const data = [
-    { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
-    { name: "Page B", uv: 800, pv: 2400, amt: 2400 },
+  const dataPie = [
+    {
+      id: "javascript",
+      label: "javascript",
+      value: 456,
+    },
+    {
+      id: "stylus",
+      label: "stylus",
+      value: 290,
+    },
   ];
 
   return (
     <div className="container h-screen">
       <div className="flex justify-center items-center h-full flex-col">
         <h1 className="text-4xl text-green-400">Hello there</h1>
-        <div>
-          <LineChart width={400} height={400} data={data}>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-            <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-          </LineChart>
+        <div className="w-full h-52 mt-4">
+          <ResponsivePie data={dataPie} />
         </div>
       </div>
     </div>
